39e9e3326769bd88c3514186a2edf9da0821aaf2
YashKachaRK <ykacha243@rku.ac.in>
2026-05-31 12:35:16 +0530

    fix

--- src/components/Singup.jsx
+++ src/components/Singup.jsx
@@ -28,6 +28,8 @@ export default function Signup() {
     }, 3000);
   };
 
+  const host = "https://i-note-book-backed.vercel.app";
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -46,7 +48,7 @@ export default function Signup() {
     } else {
       console.table(credentials);
       const response = await fetch(
-        "http://localhost:5000/api/auth/addUser",
+        `${host}/api/auth/addUser`,
         {
           method: "POST",
           headers: {
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -22,11 +22,11 @@ export default function Login() {
       [e.target.name]: e.target.value,
     });
   };
-
+const host = "https://i-note-book-backed.vercel.app";
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch(`${host}/api/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
